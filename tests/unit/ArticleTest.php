5237e768322ee571c9917580e64d147421c1113e
Alex McFarlane <alex.mcfarlane137@gmail.com>
2017-01-08 11:06:20 -0500

    add test to retrieve newest article for each category

--- tests/unit/ArticleTest.php
+++ tests/unit/ArticleTest.php
@@ -76,12 +76,67 @@ class ArticleTest extends TestCase
             'body' => 'This is my second article'
         ]);
         
-        $article2->created_at = $twoWeeksAgo;
+        $article2->created_at = $threeWeeksAgo;
         $article2->save();
         
         $articles = Article::filterBetweenDates($twoWeeksAgo, $now)->get();
 
         $this->assertTrue($articles->contains($article));
         $this->assertTrue(!$articles->contains($article2));
+    }
+
+    /** @test */
+    public function get_newest_article_for_each_category()
+    {
+        $category1 = Category::create([
+            'title' => 'PHP',
+            'description' => 'Articles related to PHP'
+        ]);
+
+        $category2 = Category::create([
+            'title' => 'JavaScript',
+            'description' => 'Articles related to JavaScript'
+        ]);
+
+        $category3 = Category::create([
+            'title' => 'Algorithms',
+            'description' => 'Trees, Sorting, Order, etc...'
+        ]);
+
+        $articlePHP = Article::create([
+            'title' => 'My PHP Article',
+            'body' => 'Article body'
+        ]);
+        $articlePHP->setCategory($category1->id);
+
+        $articleJS = Article::create([
+            'title' => 'My JS Article',
+            'body' => 'Article body'
+        ]);
+        $articleJS->setCategory($category2->id);
+
+        $articleJS2 = Article::create([
+            'title' => 'My second JS Article',
+            'body' => 'Article body'
+        ]);
+        $articleJS2->setCategory($category2->id);
+
+        $articleAlgo = Article::create([
+            'title' => 'My Algo Article',
+            'body' => 'Article body'
+        ]);
+        $articleAlgo->setCategory($category3->id);
+
+        $articleAlgo2 = Article::create([
+            'title' => 'My Second Algo Article',
+            'body' => 'Article body'
+        ]);
+        $articleAlgo2->setCategory($category3->id);
+
+        $articles = Article::newestForEachCategory();
+
+        $this->assertTrue($articles->contains($articlePHP));
+        $this->assertTrue($articles->contains($articleJS2));
+        $this->assertTrue($articles->contains($articleAlgo2));
     }
 }
