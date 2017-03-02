8fafad826d533ff992b11a168f2261a7c7968bc7
Alex McFarlane <alex.mcfarlane137@gmail.com>
2017-03-01 20:07:27 -0500

    increase varchar size of article body to 9000

--- database/migrations/2017_01_06_132943_create_articles_table.php
+++ database/migrations/2017_01_06_132943_create_articles_table.php
@@ -15,7 +15,7 @@ class CreateArticlesTable extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title');
-            $table->string('body', 9000);
+            $table->string('body', 10000);
             $table->boolean('headliner');
             $table->integer('category_id')->nullable();
             $table->timestamps();
