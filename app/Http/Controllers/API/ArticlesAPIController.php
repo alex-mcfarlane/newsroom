61d93e54aa3012c42c8c84e804a32c181a86bf4c
Alex McFarlane <alex.mcfarlane137@gmail.com>
2017-03-02 19:30:18 -0500

    add API controllers to proper namespace and folder

--- app/Http/Controllers/API/ArticlesAPIController.php
+++ app/Http/Controllers/API/ArticlesAPIController.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Http\Requests;
+use App\Http\Controllers\Controller;
 use App\Article;
 use App\Newsroom\Articles\ArticleCreator;
 use App\Newsroom\Articles\ArticleUpdater;
