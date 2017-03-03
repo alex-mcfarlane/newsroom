d126a06f7845c6deed8d33a33482d849674d53fb
Alex McFarlane <alex.mcfarlane137@gmail.com>
2017-03-02 19:31:26 -0500

    add API controllers to proper namespace and folder

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -8,23 +8,23 @@ Route::group(['prefix' => 'api'], function(){
     
     Route::get('categories/newestArticles', 'CategoriesNewestArticlesController@index');
 
-    Route::post('articles/{id}/images', 'ArticlesAPIController@addImage');
+    Route::post('articles/{id}/images', 'API\ArticlesAPIController@addImage');
     
     Route::post('articles/{id}/featured', 'FeaturedArticlesController@store');
     Route::get('articles/featured', 'FeaturedArticlesController@index');
     
-    Route::put('articles/{id}/headline', 'ArticlesAPIController@makeHeadliner');
+    Route::put('articles/{id}/headline', 'API\ArticlesAPIController@makeHeadliner');
     Route::delete('articles/{id}/feature', 'FeaturedArticlesController@removeHeadliner');
     
     Route::resource('users', 'API\UsersAPIController', ['only' =>
         'store'
     ]);
 
-    Route::resource('articles', 'ArticlesAPIController', ['only' =>
+    Route::resource('articles', 'API\ArticlesAPIController', ['only' =>
         ['store', 'update', 'index', 'show']
     ]);
 
-    Route::resource('categories', 'CategoriesAPIController', ['only' => [
+    Route::resource('categories', 'API\CategoriesAPIController', ['only' => [
         'index', 'store', 'show'
     ]]);
 });
